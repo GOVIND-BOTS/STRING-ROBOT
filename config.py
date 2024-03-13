a9573270333f48db625eed1241504f6801e9d853
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-03-13 19:40:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("22839633")
-API_HASH = getenv("5287f94935d4830bbbb242fc09ef050d")
+API_ID = int(getenv("")
+API_HASH = getenv("")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
